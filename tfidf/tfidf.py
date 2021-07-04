dd1fa2334497af799077b6d5f520eadaf8fc5a1f
Thomas Golding <thomas.golding@gmail.com>
2021-07-04 22:51:59 +0200

    tfidf with built in lemmatizer in place.

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -1,4 +1,25 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from nlpia.lemmatizer import Lemmatizer
 
 
-class 
+class TfIdf():
+    def __init__(self):
+        self.lemmatizer = Lemmatizer()
+        self.vectorizer = TfidfVectorizer(smooth_idf=True)
+        
+    def fit_transform(self, docs: list[str]):
+        docs_lemmas = self.lemmatizer.lemmatize(docs=docs)
+        x = self.vectorizer.fit_transform(raw_documents=docs_lemmas)
+        return x
+
+    def transform(self, docs: list[str]):
+        docs_lemmas = self.lemmatizer.lemmatize(docs=docs)
+        x = self.vectorizer.transform(raw_documents=docs_lemmas)
+        return x
+
+
+    
+
+
+        
