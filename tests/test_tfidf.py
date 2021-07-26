3505f7f216986251b7d566cc00c38074e05e1e2b
Thomas Golding <thomas.golding@gmail.com>
2021-07-26 10:51:09 +0200

    simple tfidf passing test

---
+++ tests/test_tfidf.py
@@ -0,0 +1,16 @@
+from tfidf.tfidf import TfIdf
+
+docs = [
+    "this is a document", 
+    "Sea food is quite nice.", 
+    "Kittens are cute."
+]
+
+categorizer = TfIdf()
+categorizer.fit_transform(docs=docs)
+
+def test_tfidf():
+    test_docs = ["food", "cute"]
+    res = categorizer.get_most_similar(docs=test_docs)
+    assert res[0] == docs[1]
+    assert res[1] == docs[2]
